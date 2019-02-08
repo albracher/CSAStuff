c1f7ffb893954ce9c0bed195b60c0f4e9017f743
albracher <allenbracher141@gmail.com>
2019-02-08 08:20:45 -0800

    part2 presentation

--- src/SecSemesterCSA/High.java
+++ src/SecSemesterCSA/High.java
@@ -4,18 +4,24 @@ public class High extends Middle {
     private String mathLvl;
     private String engLevel;
     private String sport;
+    private boolean bibleTeaching;
 
     public High(int sAge, String nName, char sGender, boolean isReader, boolean isWriter, String sMathLevel, boolean sScience, boolean sIntMath, boolean sHistory,
-                String sMathLvl, String sEngLevel, String sSport) {
+                String sMathLvl, String sEngLevel, String sSport, boolean sBibleTeaching) {
         super(sAge, nName, sGender, isReader, isWriter, sMathLevel, sScience, sIntMath, sHistory);
         mathLvl = sMathLvl;
         engLevel = sEngLevel;
         sport = sSport;
+        bibleTeaching = sBibleTeaching;
+    }
+
+    public void getMidStudent() {
+        getStudent();
+        getBasicStudent();
+        System.out.println("Knows bible teachings: " + bibleTeaching + " Intermediate math: " + getMath() + " Knows history: " + getHistory());
     }
 
     public void getHighStudent() {
-        getStudent();
-        getBasicStudent();
         getMidStudent();
         System.out.println("Math level: " + mathLvl + " English class: " + engLevel + " Sport: " + sport);
     }
