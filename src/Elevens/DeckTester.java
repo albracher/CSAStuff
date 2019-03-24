c9a74011179924a39d7641939276f4d100aa6607
albracher <allenbracher141@gmail.com>
2019-03-23 18:59:20 -0700

    asdfasdfasfsa
    aweawefawfawfwwaefawf

--- src/Elevens/DeckTester.java
+++ src/Elevens/DeckTester.java
@@ -5,17 +5,30 @@ package Elevens;
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		String[] ranks = {"jack", "queen", "king", "ace"};
-		String[] suits = {"blue", "red"};
-		int[] values = {10, 10, 10, 11};
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] rank1 = {"jack", "queen", "king"};
+        String[] suit1 = {"hearts", "spades", "diamonds"};
+        int[] value1 = {11, 12, 13};
+        Deck deck1 = new Deck(rank1, suit1, value1);
+        System.out.println("The size of Deck 1 is: " + deck1.size());
 
-		Deck cardDeck = new Deck(ranks, suits, values);
-		System.out.println(cardDeck.deal());
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+        String[] rank2 = {"jack", "queen", "king"};
+        String[] suit2 = {"clubs", "spades", "diamonds"};
+        int[] value2 = {10, 12, 13};
+        Deck deck2 = new Deck(rank2, suit2, value2);
+        System.out.println("Is Deck 2 empty? " + !deck2.isEmpty());
+
+        String[] rank3 = {"five", "eight", "queen"};
+        String[] suit3 = {"clubs", "spades", "hearts"};
+        int[] value3 = {1, 3, 12};
+        Deck deck3 = new Deck(rank3, suit3, value3);
+
+        System.out.println(deck3);
+    }
+
 }
